da8a5ffeb5ffc13ea396bd1519dce9798e7b63eb
Byron Grogan <byron4628@gmail.com>
2017-06-22 00:13:46 +0100

    fuzzy definition of chain rule

--- implicit.go
+++ implicit.go
@@ -22,4 +22,9 @@ func main() {
     b := []interface{}{}
     // define each operators properties.
     ...
+
+    // define the chain rule for a given term.
+    // let j be an idx who's element is y which is has a composite function at j+n.
+    // based on prefined rules of the composite function, compute its derivative.
+    // the expression becomes j*j+n assuming we've placed its derivative in j+n.
 }
